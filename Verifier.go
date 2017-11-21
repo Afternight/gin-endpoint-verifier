a764cb6a1c5c08f7c07448730d863abeee4f5d4d
Matthew Thomas <matt@afternight.io>
2017-11-21 19:16:22 +1100

    Added get field obtain and verify, also changed signature

--- Verifier.go
+++ Verifier.go
@@ -17,12 +17,13 @@ type FieldRequirements struct  {
 const EmailRegexString = `^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`
 const AllAcceptingRegexString = "^*"
 
-func VerifyInput(c * gin.Context, verify []FieldRequirements) (map[string]string, error) {
+//todo fix this horrible code duplication
+func ObtainVerifyPostInput(c * gin.Context, verify []FieldRequirements) (map[string]string, error) {
 	var errorStrings []string
 	finalValues := make(map[string]string)
 
 	for _,value := range verify {
-		field, exists := c.GetPostForm(value.Name)
+		field, exists := c.GetPostForm(value.Name) //todo add multiple header type support
 
 		if !exists {
 			error := fmt.Sprintf("\"%s\" is needed for this request",value.Name)
@@ -35,7 +36,35 @@ func VerifyInput(c * gin.Context, verify []FieldRequirements) (map[string]string
 			errorStrings = append(errorStrings, error)
 			continue
 		}
-		finalValues[value.Name] = field //we escape values here to prevent sql injection
+		finalValues[value.Name] = field
+	}
+
+	if len(errorStrings) > 0 {
+		return finalValues , errors.New(strings.Join(errorStrings," "))
+	} else {
+		return finalValues , nil
+	}
+}
+
+func ObtainVerifyGetInput(c * gin.Context, verify []FieldRequirements)(map[string]interface{},error){
+	var errorStrings []string
+	finalValues := make(map[string]interface{})
+
+	for _,value := range verify {
+		field := c.Query(value.Name) //todo add multiple header type support
+
+		if field == "" {
+			error := fmt.Sprintf("\"%s\" is needed for this request",value.Name)
+			errorStrings = append(errorStrings, error)
+			continue
+		}
+
+		if value.Format != nil && !value.Format.MatchString(field){
+			error := fmt.Sprintf("\"%s\" is invalid for \"%s\", should match \"%s\" ",field,value.Name,value.Format.String())
+			errorStrings = append(errorStrings, error)
+			continue
+		}
+		finalValues[value.Name] = field
 	}
 
 	if len(errorStrings) > 0 {
