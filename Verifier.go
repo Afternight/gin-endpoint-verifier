cc1d8b87715c478b5b8325d7022f1c8ec7714513
Matthew Thomas <matt@afternight.io>
2017-11-21 19:24:29 +1100

    Added header check, stanardised error handling

--- Verifier.go
+++ Verifier.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"errors"
 	"strings"
+	"net/url"
 )
 
 
@@ -17,8 +18,15 @@ type FieldRequirements struct  {
 const EmailRegexString = `^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`
 const AllAcceptingRegexString = "^*"
 
+const FormEncodedHeader = "application/x-www-form-urlencoded; charset=utf-8"
+
 //todo fix this horrible code duplication
 func ObtainVerifyPostInput(c * gin.Context, verify []FieldRequirements) (map[string]string, error) {
+	headerErr := verifyHeader(c)
+	if headerErr != nil {
+		return nil,headerErr
+	}
+
 	var errorStrings []string
 	finalValues := make(map[string]string)
 
@@ -47,8 +55,14 @@ func ObtainVerifyPostInput(c * gin.Context, verify []FieldRequirements) (map[str
 }
 
 func ObtainVerifyGetInput(c * gin.Context, verify []FieldRequirements)(map[string]interface{},error){
+	headerErr := verifyHeader(c)
+	if headerErr != nil {
+		return nil,headerErr
+	}
+
 	var errorStrings []string
 	finalValues := make(map[string]interface{})
+
 
 	for _,value := range verify {
 		field := c.Query(value.Name) //todo add multiple header type support
@@ -80,4 +94,19 @@ func GetEmailRegex() * regexp.Regexp {
 
 func GetGeneralRegex() * regexp.Regexp {
 	return regexp.MustCompile(AllAcceptingRegexString)
+}
+
+func HandleRequestErrors(c *gin.Context, code int,err error) {
+	v := url.Values{}
+	v.Add("error",err.Error())
+	c.String(code,v.Encode())
+}
+
+func verifyHeader(c *gin.Context) (error) {
+	//add future supported headers here?
+	if c.GetHeader("Content-Type") != FormEncodedHeader {
+		return errors.New("Content Type invalid, must be Form encoded")
+	} else {
+		return nil
+	}
 }
